41ebb8804c7cbbb2335e8b265b3b0ff9c4686aab
SolomonOjigbo <solomonojigbo@gmail.com>
2022-10-31 15:30:14 +0100

    Added an Infobar component at the top of the Navbar on the homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Infobar from "../../components/Infobar/Infobar";
 import Navbar from "../../components/Navbar";
 
 const Home = () => {
 	return (
 		<div>
+			<Infobar />
 			<Navbar />
 		</div>
 	);
