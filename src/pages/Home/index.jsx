f770934f2eb64d5b078967d210a1d1a4d9634293
SolomonOjigbo <solomonojigbo@gmail.com>
2022-11-01 08:52:30 +0100

    Added the Slider component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Infobar from "../../components/Infobar/Infobar";
 import Navbar from "../../components/Navbar";
+import Slider from "../../components/Slider";
 
 const Home = () => {
 	return (
 		<div>
 			<Infobar />
 			<Navbar />
+			<Slider />
 		</div>
 	);
 };
