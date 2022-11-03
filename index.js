52c6a08a6ebec0a2dfbc60f1a9f69281fbbef9c8
SolomonOjigbo <solomonojigbo@gmail.com>
2022-11-03 16:34:25 +0100

    Created the cart route with various rest-api endpoints

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const orderRoute = require("./routes/order");
+const cartRoute = require("./routes/cart");
+const cors = require("cors");
 
 mongoose
 	.connect(process.env.DATABASE_URL)
@@ -17,10 +19,13 @@ mongoose
 		console.log(err);
 	});
 
+app.use(cors());
+app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log("Backend server is running");
