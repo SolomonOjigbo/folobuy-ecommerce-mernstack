cc7eba850811d00720a749bcce15f078facd7c38
SolomonOjigbo <solomonojigbo@gmail.com>
2022-11-03 15:58:37 +0100

    Created the productRoute and orderRoute with the various api endpoints

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const orderRoute = require("./routes/order");
 
 mongoose
 	.connect(process.env.DATABASE_URL)
@@ -14,7 +17,10 @@ mongoose
 		console.log(err);
 	});
 
-app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log("Backend server is running");
